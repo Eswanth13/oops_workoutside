0239b081542bb07d61a4187a167d393f747b7d03
Eswanth13 <eswamth1304@gmail.com>
2023-07-10 18:59:28 +0530

    practice

--- practice.py
+++ practice.py
@@ -60,20 +60,7 @@
 # print(factorial)
 
 
-#----prime---
-num=int(input('enter the number: '))
-check=False
-if num==1:
-    print(f'{num} is not a prime')
-elif num>1:
-    for i in range(2,num):
-        if num%i==0:
-            check=True
-            break
-    if check==True:
-        print(f"{num} is not a prime number")
-    else:
-        print(f"{num} is prime number")
+
 
 
 
